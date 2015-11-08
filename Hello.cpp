8b825854d81cc5bcf86455a1f208c82594b5f194
AulisKerlander <jomuas@hotmail.com>
2015-11-08 18:49:54 +0200

    Hello.cpp:n jatkamista

--- Hello.cpp
+++ Hello.cpp
@@ -5,9 +5,28 @@
 
 using namespace std;
 
+HelloWorld::HelloWorld()
+{
+
+}
+
+HelloWorld::~HelloWorld()
+{
+
+}
+
 void HelloWorld::hello()
 {
 	string helloString;
-	cout << "Say hello!" << endl;
+	cout << "Say hello" << endl;
 	cin >> helloString;
+	cout << endl;
+	if (helloString == "hello")
+	{
+		cout << "Hello" << endl;
+	}
+	else
+	{
+		cout << "You didnt say hello" << endl;
+	}
 }
